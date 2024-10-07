50648e09c4db841d7e626f126327be57a8bff4c1
adri.abad.limon <adri.abad.limon@gmail.com>
2024-10-07 03:34:03 -0300

    feature/added_new_query_column

--- src/infrastructure/exploratory_analysis/trips_offers_last_month.sql
+++ src/infrastructure/exploratory_analysis/trips_offers_last_month.sql
@@ -1,6 +1,8 @@
 SELECT
+    DATE_TRUNC(DATE(departure_date_time), MONTH) AS date,
     COUNT(trip_id) as trips_count,
     SUM(total_distance_km) total_km
 FROM `develop-431503.carpool_engine.dim_trip`
 WHERE departure_date_time >= TIMESTAMP(DATE_TRUNC(DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH), MONTH))
   AND departure_date_time < TIMESTAMP(DATE_TRUNC(CURRENT_DATE(), MONTH))
+GROUP BY 1
